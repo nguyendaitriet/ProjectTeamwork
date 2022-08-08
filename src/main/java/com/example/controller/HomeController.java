1f0e53d905d724e674eec79af110c3102b6aeb36
Hân Nguyễn <hannguyen7796@gmail.com>
2022-08-08 09:11:43 +0700

    first commit

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -11,6 +11,7 @@ public class HomeController {
 
     @GetMapping
     public ModelAndView showHomePage() {
+        System.out.println("Han has added this line.");
         return new ModelAndView("/home");
     }
 }
